b811f4b0f8da8ac8abb00a1dff7e7d91c7a59936
AghasiPOTOYAN <aghasi.potoyan@tumo.org>
2023-11-19 15:18:51 +0400

    kkkk

--- gameOfLife-Aghasi Potoyan/client/script.js
+++ gameOfLife-Aghasi Potoyan/client/script.js
@@ -1,80 +1,21 @@
 let socket = io()
+let grassColor = "green"
 
+let ashunButton = document.getElementById("ashun");
+ashunButton.addEventListener("click", handleAsunClick)
 
-// function matrixGenerator(matrixSize, grass, grassEater, predator, jur, shark, jellyfish) {
-//     var matrix = []
+function handleAsunClick() {
+    alert("ashuuunn")
+    grassColor = "orange"
+}
+let dzmerButton = document.getElementById("dzmer");
+ashunButton.addEventListener("click", handleDzmerClick)
 
-//     for (let i = 0; i < matrixSize; i++) {
-//         matrix.push([])
-//         for (let j = 0; j < matrixSize; j++) {
-//             matrix[i].push(0)
+function handleDzmerClick() {
+    alert("dzmeeeerr")
+    grassColor = "white"
+}
 
-//         }
-//     }
-
-
-//     for (let i = 0; i < grass; i++) {
-
-//         var x = Math.floor(Math.random() * matrixSize)
-//         var y = Math.floor(Math.random() * matrixSize)
-
-//         matrix[y][x] = 1
-
-//     }
-
-//     for (let i = 0; i < grassEater; i++) {
-
-//         var x = Math.floor(Math.random() * matrixSize)
-//         var y = Math.floor(Math.random() * matrixSize)
-
-//         matrix[y][x] = 2
-
-//     }
-
-
-
-//     for (let i = 0; i < predator; i++) {
-
-//         var x = Math.floor(Math.random() * matrixSize)
-//         var y = Math.floor(Math.random() * matrixSize)
-
-//         matrix[y][x] = 3
-
-
-//     }
-
-//     for (let i = 0; i < jur; i++) {
-
-//         var x = Math.floor(Math.random() * matrixSize)
-//         var y = Math.floor(Math.random() * matrixSize)
-
-//         matrix[y][x] = 4
-
-
-//     }
-//     for (let i = 0; i < shark; i++) {
-
-//         var x = Math.floor(Math.random() * matrixSize)
-//         var y = Math.floor(Math.random() * matrixSize)
-
-//         matrix[y][x] = 5
-
-
-//     }
-//     for (let i = 0; i < jellyfish; i++) {
-
-//         var x = Math.floor(Math.random() * matrixSize)
-//         var y = Math.floor(Math.random() * matrixSize)
-
-//         matrix[y][x] = 6
-
-
-//     }
-
-//     return matrix
-// }
-
-// var matrix = matrixGenerator(30, 40, 15, 5, 50, 39, 20)
 let side = 25
 //
 
@@ -83,30 +24,6 @@ function setup() {
     frameRate(15)
     createCanvas(50 * side, 50 * side)
 
-//     for (let y = 0; y < matrix.length; y++) {
-//         for (let x = 0; x < matrix[y].length; x++) {
-
-//             if (matrix[y][x] == 1) {
-//                 var gr = new Grass(x, y)
-//                 grassArr.push(gr)
-//             } else if (matrix[y][x] == 2) {
-//                 var grEat = new GrassEater(x, y)
-//                 grassEaterArr.push(grEat)
-//             } else if (matrix[y][x] == 3) {
-//                 var pred = new Predator(x, y)
-//                 predatorArr.push(pred)
-//             } else if (matrix[y][x] == 4) {
-//                 var jur = new Jur(x, y)
-//                 jurArr.push(jur)
-//             } else if (matrix[y][x] == 5) {
-//                 var shark = new Shark(x, y)
-//                 sharkArr.push(shark)
-//             } else if (matrix[y][x] == 6) {
-//                 var jellyfish = new Jellyfish(x, y)
-//                 jellyfishArr.push(jellyfish)
-//             }
-//         }
-//     }
 
 }
 
@@ -116,11 +33,17 @@ function changeColor(matrix) {
     for (let y = 0; y < matrix.length; y++) {
         for (let x = 0; x < matrix[y].length; x++) {
             var toBot = side - side * 0.5
-            if (matrix[y][x] == 1) {
-                fill("green")
+            if (matrix[y][x] == 1 && grassColor == "green") {
+                fill(grassColor)
                 rect(x * side, y * side, side, side)
-                text('🌿', x * side, y * side + toBot)
-            } else if (matrix[y][x] == 2) {
+                text('🍀', x * side, y * side + toBot)
+            } else if (matrix[y][x] == 1 && grassColor == "orange") {
+                fill(grassColor)
+
+                rect(x * side, y * side, side, side)
+                text('🍁', x * side, y * side + toBot)
+
+            }else if (matrix[y][x] == 2) {
                 fill("white")
                 rect(x * side, y * side, side, side)
                 text('🍾', x * side, y * side + toBot)
@@ -149,30 +72,7 @@ function changeColor(matrix) {
         }
     }
 
-    // for (let i in grassArr) {
-    //     grassArr[i].mul()
-    // }
 
-    // for (let i in grassEaterArr) {
-    //     grassEaterArr[i].eat()
-
-    // }
-    // console.log(predatorArr.length);
-    // for (let i in predatorArr) {
-    //     predatorArr[i].eat()
-    // }
-
-
-    // for (let i in jurArr) {
-    //     jurArr[i].eat()
-    // }
-
-    // for (let i in sharkArr) {
-    //     sharkArr[i].eat()
-    // }
-    // for (let i in jellyfishArr) {
-    //     jellyfishArr[i].eat()
-    // }
 }
 
-socket.on("send matrix",changeColor)
+socket.on("send matrix", changeColor)
